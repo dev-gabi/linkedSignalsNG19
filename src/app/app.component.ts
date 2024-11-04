fca3317cfffcf0b67955421b9b96043260d4c351
gabi <gabrielsh@matrix.co.il>
2024-11-04 09:08:43 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, linkedSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-    
+    $names = signal<string[]>([]);
+    $namesCount = linkedSignal(()=> this.$names().length);
 
+    addName(name:string){
+      this.$names.update(names=>[...names, name]);S
+    }
 }
